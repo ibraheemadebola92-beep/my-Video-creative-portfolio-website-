88f4dfaa10a284f9a3ab303e8a7b2c2a86722714
ibraheemadebola92-beep <ibraheemadebola92@gmail.com>
2026-09-16 15:14:41 +0100

    Create next.config.js

---
+++ scy-ai-visual-creator/next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+
+  experimental: {
+    typedRoutes: false,
+  },
+};
+
+module.exports = nextConfig;
